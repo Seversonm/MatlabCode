f2d77d743851a9303dbe19c156ef092a713ee4ca
Seversonm <seversonmandi@gmail.com>
2020-02-03 14:37:34 -0600

    Update Mandi_analysis 2/3

--- Mandi_analysis.m
+++ Mandi_analysis.m
@@ -1,15 +1,9 @@
-
 st = size(unique(data.stim(:,2)'),2); %% finds the number of unique stimuli played in this program. 
 for i = 1:st %% for each unique stimuli
-    inds(:,i) = find(data.stim(:,2)==i);%% creates an array. Finds the positions in the original data.stim file where this stimulus was played. Inds means indices. 
+    inds(:,i) = find(data.stim(:,2)==i);%% creates an array. Finds the positions in the original data.stim file where this stimulus was played.
     frames(:,i) = data.stim(inds(:,i),1);%% creates an array. Finds the values (frame in which stimulus started) that go with each position in inds.
-
-    %frames(:,i) = data.stim((find(data.stim(:,2)==i),1); This is a line
-    %you could use if you wanted to combine the previous two lines of code
-    %and don't need to save inds for anything. 
-
 end
-nt = length(frames(:,1)); %% gives the length of the frames array (ex. if there are 50 instances when stimuli started playing, this value would be 50.)
+nt = length(frames(:,1)); %% gives the length of the frames array (number of repeats).
 cellnum = length(data.Fraw(:,1)); %% Tells us how many rows of data there are in data.Fraw. This corresponds to how many cells were circled. 
 for s = 1:st %% make a matrix with stim number, repeats, and time. This means for each unique stimuli.
     for n = 1:nt %% and for each frame in which the stimulus started
@@ -19,5 +13,123 @@ for s = 1:st %% make a matrix with stim number, repeats, and time. This means fo
     end
 end
 
-%F0= response?
-%Dfof = (F(t)-F0)/F0
+%%%%%%STUFF YOU NEED TO EDIT%%%%%%%%
+ce = cellnum-1; %we don't care about the blank
+el = data.stim(:,2);
+el = el'; 
+%%%%%%%%%%Set some times%%%%%%%%%%%%
+del =0; % time from ttl->show
+bas = del+32; %for delta F
+en = 352; %end of stim + some more
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%preallocate variables%%%%%%%
+combFraw = zeros(nt,((en-del)+1),st);
+combf = zeros(nt,((en-del)+1),st);
+degmean = zeros(st,1);
+degstd = zeros(st,1);
+degstderr = zeros(st,1);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for b = 1:ce %cell number
+    w = cellnum; % blank dark cell
+    n = nt;
+for p = 1:n %repeat number
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+%%%%%%raw signal for each trial%%%%%    
+for i = 1:st
+    combFraw(p,:,i) = data.Fraw(b,frames(p,i)+del:frames(p,i)+en)-data.Fraw(w,frames(p,i)+del:frames(p,i)+en);                   
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+end
+
+%%%%%%%%%%delta F calc%%%%%%%%%%%%%%
+for p=1:n
+     for i=1:st
+combf(p,:,i)=(combFraw(p,:,i)-mean(combFraw(p,del:bas,i)))./mean(combFraw(p,del:bas,i));
+     end
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%frames to seconds%%%%%%%%%%%%
+time(1) = 1/32; %32 frames/s
+for i =2:((en-del)+1) % 2 to end of frame
+    time(i) = time(i-1) + 1/32;
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%create trial averages%%%%%%%%%%
+combfa = mean(combf,1);
+combFrawa = mean(combFraw,1);
+combstd = std(combFraw,1);
+combstderr = combstd/(sqrt(ce));
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%Plot Raw data%%%%%%%%%%%%%% 
+%  figure (b+200)
+%  for j=1:st %num of stim. condition
+%  subplot(st,1,j)
+%     for i=1:n          
+%  plot(time,combFraw(i,:,j),'k')
+%     hold on
+%  plot(time,combFrawa(:,:,j),'r')
+% %     ylim([-1 1])
+%      xlim([0 11])
+%      line([1.5 1.5],[0 200]) %stim onset
+%     hold on
+%      line([5.5 5.5],[0 200])
+%set(gcf, 'Position', [100, 100, 400, 850])
+%     end
+%  end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%Plot Delta F%%%%%%%%%%%%%%
+figure (b+300)
+  for j=1:st %num of stim. condition
+  H(j)=subplot(st,1,j); %H contains axis handle
+  hold on
+     for i=1:n          
+  plot(time,combf(i,:,j),'k')
+  hold on
+  plot(time,combfa(:,:,j),'r')
+  hold on
+ %ylim([-20 20])
+xlim([0 11])
+%line([1.5 1.5],[-1 1]) %stim onset
+ % hold on
+%line([5.5 5.5],[-1 1]) %stim offset
+set(gcf, 'Position', [100, 100, 400, 850])
+ end
+ end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%plot tuning curve%%%%%%%%%%
+%Tuning curve from raw signal
+% for i = 1:st
+%     degmean(i,1) = mean(combFrawa(1,128:256,i));
+% end
+% degmean = degmean';
+% degmeanf = degmean(1,1:8);
+% for i = 1:st
+%      degstderr(i,1) = mean(combstd(1,128:256,i));
+% end 
+% degstderr = degstderr';
+% degstderrf = degstderr(1,1:8);
+% ax = [0,45,90,135,180,225,270,315];
+% xlabels = {'0°','45°','90°','135°','180°','225°','270°','315°'};
+% figure(b+400)   
+%      errorbar(ax,degmeanf,degstderrf)
+%      set(gca, 'Xtick', ax, 'XtickLabel', xlabels)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+end
+
+%%%%%%%%%Clean Workspace%%%%%%%%%%%%
+%clear a ax b bas bl ce degmean degstd del el en i l n p w st rp xlabels y z
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% F(t) = F(t) - Fo /Fo
